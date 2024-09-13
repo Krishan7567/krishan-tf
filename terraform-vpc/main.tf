320cc30f7b99e414b6c228eb65627be8fb9e0e72
unknown <krishanb281988@gmail.com>
2024-09-13 19:03:02 +0530

    tt

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -1,4 +1,4 @@
 module "vpc" {
-  source    = "https://github.com/Krishan7567/krishan-tf/terraform-module-vpc.git"
+  source    = "https://github.com/Krishan7567/krishan-tf.git"
   VPC_CIDR  =  var.VPC_CIDR
 }
